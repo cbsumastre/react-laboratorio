4c3579abb7fdfc3e08be47f358defc67a91f5059
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2025-03-25 19:32:07 +0100

    chore - changes in footer

--- apps/listado-imagenes/src/footer.tsx
+++ apps/listado-imagenes/src/footer.tsx
@@ -4,7 +4,7 @@ import heartIcon from "./assets/heart_icon.svg";
 
 export const Footer: React.FC = () => {
     return <footer>
-        <div>Made with  <img src={heartIcon} alt="love" title="love" /> by <a href="https://github.com/cbsumastre" target="_blank">@cbsumastre</a> in Madrid 03/2025</div>
         <div>Máster Front End Lemoncode XVII - Módulo 4.1 - Frameworks - React Laboratorio Listado de imágenes</div>
+        <div>Made with  <img src={heartIcon} alt="love" title="love" /> by <a href="https://github.com/cbsumastre" target="_blank">@cbsumastre</a> in Madrid 03/2025</div>
     </footer>
 }
