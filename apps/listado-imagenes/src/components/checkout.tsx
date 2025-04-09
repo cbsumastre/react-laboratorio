9b46606b3ca619162134c5759f66cdebed4cce3e
Carlos Blázquez Sumastre <cbsumastre@gmail.com>
2025-04-09 18:28:48 +0200

    feat - reset checkout data

--- apps/listado-imagenes/src/components/checkout.tsx
+++ apps/listado-imagenes/src/components/checkout.tsx
@@ -9,18 +9,31 @@ interface Props {
 }
 export const Checkout: React.FC<Props> = (props) => {
     const { isOpen, onConfirm, onCancel } = props
-    
-    const [titular, setTitular ] = React.useState("")
-    const [numero, setNumero ] = React.useState("")
+
+    const [titular, setTitular] = React.useState("")
+    const [numero, setNumero] = React.useState("")
     const [fechaCaducidad, setFechaCaducidad] = React.useState("")
-    const [ cvc, setCvc] = React.useState("")
+    const [cvc, setCvc] = React.useState("")
+
+    const reset = React.useCallback(()=>{
+        setTitular("")
+        setNumero("")
+        setFechaCaducidad("")
+        setCvc("")
+    },[])
 
     const handleSubmit = (e: React.FormEvent) => {
         // aquí habría que realizar validaciones
         e.preventDefault()
         alert("Compra realizada con exito")
+        reset();
         onConfirm()
-      }
+    }
+
+    const handleCancel = () => {
+        reset();
+        onCancel();
+    }
 
 
     const shopContext = React.useContext(ShopContext)
@@ -44,36 +57,36 @@ export const Checkout: React.FC<Props> = (props) => {
 
                         <div className="group">
                             <label htmlFor="titular">Titular</label>
-                            <input id="titular" type="text" placeholder="Titular de la cuenta" value={titular} required onChange={(e)=>{
+                            <input id="titular" type="text" placeholder="Titular de la cuenta" value={titular} required onChange={(e) => {
                                 setTitular(e.target.value)
-                            }}/>
+                            }} />
                         </div>
 
                         <div className="group">
                             <label htmlFor="numero">Número</label>
-                            <input id="cardNumber" type="text" placeholder="1234 5678 9012 3456" required value={numero} onChange={(e)=>{
+                            <input id="cardNumber" type="text" placeholder="1234 5678 9012 3456" required value={numero} onChange={(e) => {
                                 setNumero(e.target.value)
-                            }}/>
+                            }} />
                         </div>
 
                         <div className="row">
                             <div className="group">
                                 <label htmlFor="fecha">Fecha de caducidad</label>
-                                <input id="fecha" type="text" placeholder="MM/YY" required value={fechaCaducidad} onChange={(e)=>{
+                                <input id="fecha" type="text" placeholder="MM/YY" required value={fechaCaducidad} onChange={(e) => {
                                     setFechaCaducidad(e.target.value)
-                                }}/>
+                                }} />
                             </div>
                             <div className="group">
                                 <label htmlFor="cvc">CVC</label>
-                                <input id="cvc" type="text" placeholder="123" required value={cvc} onChange={(e)=>{
+                                <input id="cvc" type="text" placeholder="123" required value={cvc} onChange={(e) => {
                                     setCvc(e.target.value)
-                                }}/>
+                                }} />
                             </div>
                         </div>
                     </section>
 
                     <div className="modal-footer modal-buttons">
-                        <button type="button" className="cancel-button" onClick={onCancel}>
+                        <button type="button" className="cancel-button" onClick={handleCancel}>
                             Cancelar
                         </button>
                         <button type="submit" className="confirm-button">
